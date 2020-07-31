2d42ac84e0afea9ceed46c6bf8390b6df3bfaea0
Ryan Milne <ryanmilne101@gmail.com>
2020-07-31 11:55:43 -0600

    Backend done

---
+++ server/ctrl.js
@@ -0,0 +1,38 @@
+const sayings = require("../sayings.json");
+let id = 6;
+
+module.exports = {
+  getSaying: (req, res) => {
+    res.status(200).send(sayings);
+  },
+  addSaying: (req, res) => {
+    const { saying } = req.body;
+    const newSaying = {
+      id,
+      saying,
+      edit: false,
+    };
+    id++;
+    sayings.push(newSaying);
+    res.status(200).send(sayings);
+  },
+  deleteSaying: (req, res) => {
+    const { id } = req.params;
+    const i = sayings.findIndex((el) => el.id === +id);
+    if (i === -1) {
+      res.sendStatus(404);
+    } else {
+      sayings.splice(i, 1);
+      return res.status(200).send(sayings);
+    }
+  },
+  editSaying: (req, res) => {
+    const { id } = req.params;
+    const { saying } = req.body;
+    const index = sayings.findIndex((el) => {
+      return el.id === +id;
+    });
+    sayings[index].saying = saying;
+    return res.status(200).send(sayings);
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const saying = require("../saying.json");
+const sayings = require("../sayings.json");
 const app = express();
+const ctrl = require("./ctrl");
 app.use(express.json());
 
-
-
-
+app.get("/api/sayings", ctrl.getSaying);
+app.post("/api/sayings", ctrl.addSaying);
+app.delete("/api/sayings/:id", ctrl.deleteSaying);
+app.put("/api/sayings/:id", ctrl.editSaying);
 
 const port = 5000;
-app.listen(port, `I will survive on ${5000}`);
+app.listen(port, () => console.log(`I will survive on ${5000}`));
