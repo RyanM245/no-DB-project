7a88bb81e55b8216f1cc4fc448a54ae71ebe4273
Ryan Milne <ryanmilne101@gmail.com>
2020-08-03 08:50:26 -0600

    Header Gif

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className = "header-container">
             <img 
-            src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS-2nVGZzoFCRlfrob7aR4yzlV-agSnPZceJw&usqp=CAU"
+            src = "https://media.discordapp.net/attachments/727541480243331142/738788661557985371/Adam_Singing.gif"
             alt = "Adam"
             className = "img-logo"/>
             <div className = "header-title"> Adam's Happy Place </div>
